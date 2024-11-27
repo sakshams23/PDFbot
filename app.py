601c6b2a5efa47d8d2bcefd5a9a9fdeeb859c7e3
Saksham Soni <sonisaksham20208@gmail.com>
2024-11-27 16:04:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,106 +1,148 @@
-import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-import google.generativeai as genai
-from langchain.vectorstores import FAISS
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
-from dotenv import load_dotenv
+import io
+import PyPDF2  # Library to read PDF files
+import pytesseract  # Library for optical character recognition (OCR)
+from PIL import Image  # Library for image processing
+import streamlit as st  # Streamlit library for building web applications
+from dotenv import load_dotenv  # Library to load environment variables
+import google.generativeai as gen_ai  # Google Generative AI library
+from langchain_community.vectorstores import FAISS  # Vector store for efficient similarity search
+from langchain.prompts import PromptTemplate  # Library for creating prompts
+from langchain.text_splitter import RecursiveCharacterTextSplitter  # Library for splitting text into chunks
+from langchain_google_genai import GoogleGenerativeAIEmbeddings  # Embeddings for Google Generative AI
+from langchain_google_genai import ChatGoogleGenerativeAI  # Google Generative AI model for chatbot
+from langchain.chains.question_answering import load_qa_chain  # Library for question-answering chains
 
-load_dotenv()
-os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+load_dotenv()  # Load environment variables
+os.getenv("GOOGLE_API_KEY")  # Get Google API key from environment variables
+gen_ai.configure(api_key=os.environ["GOOGLE_API_KEY"])  # Configure Google Generative AI with API key
+st.set_page_config(page_title="PdfOcrBot", page_icon=":book:")  # Set Streamlit app title and icon
 
+# Initialize session state variables
+if 'vector_store' not in st.session_state:
+    st.session_state['vector_store'] = None
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = []
 
+# Tesseract File Path
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Harsh\Desktop\Slack_Bot\Tesseract-OCR\tesseract.exe'
 
+# Function to extract text from PDF and image files (with optional OCR for PDF)
+def extract_text_from_files(file_bytes_list, file_extension_list, ocr=False):
+    text = ""
 
+    for file_bytes, file_extension in zip(file_bytes_list, file_extension_list):
+        if file_extension == ".pdf":
+            pdf_reader = PyPDF2.PdfReader(io.BytesIO(file_bytes))
+            for page_num in range(len(pdf_reader.pages)):
+                page_obj = pdf_reader.pages[page_num]
+                if ocr:
+                    images = page_obj.images
+                    if images:
+                        for image in images:
+                            image_bytes = image.data
+                            image_file = io.BytesIO(image_bytes)
+                            image = Image.open(image_file)
+                            text += pytesseract.image_to_string(image) + "\n"
+                text += page_obj.extract_text()
 
+        elif file_extension in [".png", ".jpg", ".jpeg", ".webp"]:
+            image_file = io.BytesIO(file_bytes)
+            image = Image.open(image_file)
+            text += pytesseract.image_to_string(image) + "\n"
 
-def get_pdf_text(pdf_docs):
-    text=""
-    for pdf in pdf_docs:
-        pdf_reader= PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text+= page.extract_text()
-    return  text
+    return text
 
-
-
-def get_text_chunks(text):
+# Function to split text into chunks for vector store
+def split_text_into_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
-    chunks = text_splitter.split_text(text)
-    return chunks
+    text_chunks = text_splitter.split_text(text)
+    return text_chunks
 
+# Function to create a vector store from text chunks
+def create_vector_store(chunks):
+    if not chunks:
+        st.warning("No text content found in the uploaded file.")
+        return
 
-def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    vector_store.save_local("faiss_index")
+    try:
+        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+        vector_store = FAISS.from_texts(chunks, embedding=embeddings)
+        st.session_state['vector_store'] = vector_store
+    except Exception as e:
+        st.error(f"An error occurred while creating the vector store: {str(e)}")
 
-
-def get_conversational_chain():
-
+# Function to set up the conversational chain for question-answering
+def setup_conversational_chain():
     prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
-    Context:\n {context}?\n
-    Question: \n{question}\n
+    Answer the question in a short, precise, detailed, friendly and engaging way, drawing from the provided context if possible. If the question is not directly related to the context, provide a thoughtful and relevant response based on your general knowledge.
+    Make sure the response is in common English and appears human-like, avoiding any patterns or language that could be identified as AI-generated by AI text detectors.
+
+    Context:{context}
+
+    Question:{question}
 
     Answer:
     """
 
-    model = ChatGoogleGenerativeAI(model="gemini-pro",
-                             temperature=0.3)
-
-    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
-    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
-
+    model = ChatGoogleGenerativeAI(model="gemini-pro", client=gen_ai, temperature=0.7)
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    chain = load_qa_chain(llm=model, chain_type="stuff", prompt=prompt)
     return chain
 
+# Function to get a response to a user question
+def get_response(user_question):
+    relevant_docs = st.session_state['vector_store'].similarity_search(user_question)
+    conversational_chain = setup_conversational_chain()
+    response = conversational_chain({"input_documents": relevant_docs, "question": user_question}, return_only_outputs=True)
+    return response
 
-
-def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    
-    new_db = FAISS.load_local("faiss_index", embeddings)
-    docs = new_db.similarity_search(user_question)
-
-    chain = get_conversational_chain()
-
-    
-    response = chain(
-        {"input_documents":docs, "question": user_question}
-        , return_only_outputs=True)
-
-    print(response)
-    st.write("Reply: ", response["output_text"])
-
-
-
-
+# Main function
 def main():
-    st.set_page_config("Chat PDF")
-    st.header("Chat with PDF using Gemini💁")
+    st.title("PdfOcrBot")
+    st.write("Welcome to PdfOcrBot! I can process multiple PDF and image files for you. Upload your files, and I'll extract the text. You can then ask me questions about the files' content.")
 
-    user_question = st.text_input("Ask a Question from the PDF Files")
+    uploaded_files = st.file_uploader("Upload your PDF or Image files", type=["pdf", "png", "jpg", "jpeg", "webp"], accept_multiple_files=True)
+    ocr = st.checkbox("Enable OCR for PDF files")
 
-    if user_question:
-        user_input(user_question)
+    if st.button("Clear Conversation"):
+        st.session_state['messages'].clear()
+        st.session_state['vector_store'] = None
+        st.experimental_rerun()
 
-    with st.sidebar:
-        st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
-        if st.button("Submit & Process"):
-            with st.spinner("Processing..."):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                get_vector_store(text_chunks)
-                st.success("Done")
+    if uploaded_files:
+        file_bytes_list = [uploaded_file.read() for uploaded_file in uploaded_files]
+        file_extension_list = [os.path.splitext(uploaded_file.name)[1].lower() for uploaded_file in uploaded_files]
 
+        for uploaded_file in uploaded_files:
+            file_extension = os.path.splitext(uploaded_file.name)[1].lower()
+            if file_extension in [".png", ".jpg", ".jpeg", ".webp"]:
+                with st.expander("Display Image"):
+                    st.image(uploaded_file)
 
+        st.write("Processing your files...")
+        file_text = extract_text_from_files(file_bytes_list, file_extension_list, ocr)
+        text_chunks = split_text_into_chunks(file_text)
+        create_vector_store(text_chunks)
+        st.write("Files processed successfully!")
+
+    if st.session_state['vector_store'] is not None:
+        for message in st.session_state['messages']:
+            with st.chat_message(message["role"]):
+                st.write(message["content"])
+
+        if prompt := st.chat_input("Ask me anything about the file's content", key="chat_input"):
+            st.session_state['messages'].append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.write(prompt)
+
+            with st.chat_message("assistant"):
+                with st.spinner("Thinking..."):
+                    response = get_response(prompt)
+                    full_response = ''.join(response['output_text'])
+                    st.write(full_response)
+                    message = {"role": "assistant", "content": full_response}
+                    st.session_state['messages'].append(message)
 
 if __name__ == "__main__":
     main()
